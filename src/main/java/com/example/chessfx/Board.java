b45fe9f78adc68e362fbf7d6f936c85baf19d4cf
ThousandYearGrind <patrickd9900@gmail.com>
2025-09-04 19:32:47 -0400

    adding documentation and improving encapsulation

--- src/main/java/com/example/chessfx/Board.java
+++ src/main/java/com/example/chessfx/Board.java
@@ -1,28 +1,37 @@
 package com.example.chessfx;
 
 import com.example.chessfx.pieces.*;
-import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Board {
 
     private final Pane pane;
-    private Draw d;
+    private final Draw d;
     private final Tile[][] board;
 
-    /* functionality:
-     * contain a 2D board of tiles with rows and columns
-     * when a board is constructed, init a board of tiles and default pieces for both colors on the appropriate tiles
+    /**
+     * set the pane to display in
+     * initialize 8x8 array of Tile objects <br>
+     * create the Draw object based on the board to
+     * @param pane - the pane the board will be displayed in
      */
     public Board(Pane pane) {
         this.pane = pane;
         board = new Tile[8][8];
+        // Draw must be constructed after board is initialized (has a reference to board
         this.d = new Draw(this);
         initTiles();
         initPieces();
     }
 
+    /**
+     * > populate the 2D array of Tile objects with tiles of the appropriate color and size <br>
+     * > (x,y) within the array represents rank x + 1, file y + 1 <br>
+     */
     private void initTiles() {
         boolean isDark = true;
         for (int row = 0; row < 8; row++) {
@@ -34,6 +43,14 @@ public class Board {
         }
     }
 
+    /**
+     * > populate the 2D array of Tile objects with the proper initial Piece objects <br>
+     * > rank 1 will have the white back rank <br>
+     * > rank 2 has the white pawns <br>
+     * > rank 7 has the black pawns <br>
+     * > rank 8 has the black back rank <br>
+     * > pointers to the pieces will be stored within the Tile objects <br>
+     */
     private void initPieces() {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
@@ -82,13 +99,113 @@ public class Board {
 //        }
 //    }
 
-   public Pane getGUIBoard() {
+    /**
+     * > first clear the current GUI display of Nodes <br>
+     * > add each element in the board again to reflect any changes that were made <br>
+     */
+    private void draw() {
        pane.getChildren().clear();
        pane.getChildren().add(d.drawBoard());
-       return pane;
-   }
+    }
 
     public Tile[][] getBoard() {
         return board;
+    }
+
+    private static class Draw {
+        // make a pane with the board
+        Board b;
+        Tile[][] board;
+
+        public Draw(Board b) {
+            this.b = b;
+            board = b.getBoard();
+        }
+
+        public Pane drawBoard() {
+            Pane boardImage = new Pane();
+            for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    Tile t = board[row][col];
+                    Rectangle tileRectangle = drawTile(t);
+                    tileRectangle.setTranslateX(Tile.getWidth() * col);
+                    tileRectangle.setTranslateY(Tile.getWidth() * row);
+                    boardImage.getChildren().add(tileRectangle);
+                }
+            }
+
+            for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    Tile t = board[row][col];
+                    if (t.getPiece() != null) {
+                        ImageView pcImg = drawPiece(t.getPiece());
+                        pcImg.setTranslateX(Tile.getWidth() * col);
+                        pcImg.setTranslateY(Tile.getWidth() * row);
+                        boardImage.getChildren().add(pcImg);
+                    }
+                }
+            }
+            return boardImage;
+        }
+
+        // make an ImageView of the Piece
+        private ImageView drawPiece(Piece p) {
+            String path = "/pieceImages/";
+            if (p.getColor() == Player.WHITE)
+                path += "w-";
+            else
+                path += "b-";
+            path += p.getType().toString().toLowerCase();
+            path += ".png";
+            System.out.println(path);
+            ImageView pcImg = new ImageView(String.valueOf(this.getClass().getResource(path)));
+            pcImg.setPickOnBounds(true);
+            makeDraggable(pcImg);
+            return pcImg;
+        }
+
+        // make a rectangle of the Tile
+        private Rectangle drawTile(Tile t) {
+            Rectangle rect;
+            if (t.isDark()) {
+                rect = new Rectangle(Tile.getWidth(), Tile.getWidth());
+                rect.setFill(Color.SIENNA);
+                return rect;
+            }
+            rect = new Rectangle(Tile.getWidth(), Tile.getWidth());
+            rect.setFill(Color.BEIGE);
+            return rect;
+        }
+
+        private int guiRow, guiCol, endRow, endCol;
+        private Piece currentPiece;
+        private void makeDraggable(Node... s) {
+            for (Node node : s) {
+                node.setOnMousePressed(e -> {
+                    node.toFront();
+                    guiCol = (int) e.getSceneX() / 100;
+                    guiRow = (int) e.getSceneY() / 100;
+                    currentPiece = board[guiRow][guiCol].getPiece();
+                    endCol = guiCol;
+                    endRow = guiRow;
+                    node.setTranslateX(e.getSceneX() - Tile.getWidth() / 2.0);
+                    node.setTranslateY(e.getSceneY() - Tile.getWidth() / 2.0);
+                    System.out.println(guiRow + ":" + guiCol);
+                });
+                node.setOnMouseDragged(e -> {
+                    node.setTranslateX(e.getSceneX() - Tile.getWidth() / 2.0);
+                    node.setTranslateY(e.getSceneY() - Tile.getWidth() / 2.0);
+                    endRow = (int) e.getSceneY() / 100;
+                    endCol = (int) e.getSceneX() / 100;
+                });
+                node.setOnMouseReleased(mouseEvent -> {
+                    node.setTranslateX(endCol * Tile.getWidth());
+                    node.setTranslateY(endRow * Tile.getWidth());
+                    currentPiece.move(endRow, endCol);
+                    b.draw();
+                    System.out.println(endRow + ":" + endCol);
+                });
+            }
+        }
     }
 }
--- src/main/java/com/example/chessfx/ChessFX.java
+++ src/main/java/com/example/chessfx/ChessFX.java
@@ -1,8 +1,6 @@
 package com.example.chessfx;
 
 import javafx.application.Application;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -15,7 +13,7 @@ public class ChessFX extends Application {
         Pane pane = new Pane();
         Scene scene = new Scene(pane, 800, 800);
         b = new Board(pane);
-        b.getGUIBoard();
+        b.draw();
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
